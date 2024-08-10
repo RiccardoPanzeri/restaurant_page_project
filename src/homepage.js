79767fc9a6544d78328a4c6d5a5580a04667cd22
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-08-10 04:06:40 +0200

    aggiunge location, modifica testo footer

--- src/homepage.js
+++ src/homepage.js
@@ -1,6 +1,7 @@
 import italianPennant from"./images/italy_pennant.png";
 import clockIcon from "./images/chef-hat.png";
 import italianEsaGold from "./images/italy_esa_gold.png";
+import italianGpsIcon from "./images/italy_gps.png";
 
 
 function loadHomePage(){
@@ -26,32 +27,45 @@ const staffSignature = document.createElement("p");
 const hours = document.createElement("div");
 const clock = document.createElement("img");
 const hourTitle = document.createElement("h3");
+//location
+const location = document.createElement("div");
+const gpsIcon = document.createElement("img");
+const locationTitle = document.createElement("h3");
+const locationText = document.createElement("p");
 
 
 //modifica classi
 title.classList.add("title");
 logo.classList.add("logo");
 TitleHeader.classList.add("titleHeader");
-description.classList.add("homepageDiv", "description");
+description.classList.add("homepageDiv", "description", "divChildDynamic");
 pennant.classList.add("absoluteIcon");
 descriptionText.classList.add("normalText");
 staffSignature.classList.add("signature");
-hours.classList.add("homepageDiv", "hours");
+hours.classList.add("homepageDiv", "hours", "divChildDynamic");
 clock.classList.add("absoluteIcon");
 hourTitle.classList.add("subHeader");
+location.classList.add("homepageDiv", "location", "divChildDynamic");
+gpsIcon.classList.add("absoluteIcon");
+locationTitle.classList.add("subHeader");
+locationText.classList.add("normalText");
 //modifica attributi
 logo.setAttribute("src", italianEsaGold);
 pennant.setAttribute("src", italianPennant);
 clock.setAttribute("src", clockIcon);
+gpsIcon.setAttribute("src", italianGpsIcon);
 //modifica testo
 TitleHeader.textContent = 'Ristorante Pizzeria "La Vera Italia"';
 descriptionText.textContent = "Template della descrizione, testo casuale, blah blah blah blah e ancora blah e poi blah, ciao mondo";
 staffSignature.textContent = "-Lo Staff de 'La Vera Italia'";
 hourTitle.textContent = "Hours";
+locationTitle.textContent= "Location";
+locationText.textContent = "789 Colosseum Avenue, Augustus City, Your State";
 //aggiunta elementi
 contentDiv.appendChild(title);
 contentDiv.appendChild(description);
 contentDiv.appendChild(hours);
+contentDiv.appendChild(location);
 //aggiunta header
 title.appendChild(logo);
 title.appendChild(TitleHeader);
@@ -76,6 +90,10 @@ for(let i = 0; i < 7; i++){
     }
     hours.appendChild(hourP);
 }
+//aggiunta location
+location.appendChild(gpsIcon);
+location.appendChild(locationTitle);
+location.appendChild(locationText);
 }
 
 export default loadHomePage;
