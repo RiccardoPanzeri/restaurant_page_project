a7c393ffcaa6e1063baa876ab7e98644b842a62d
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-08-10 16:26:57 +0200

    crea funzioni separate per la creazione e la modifica del titolo principale

--- src/homepage.js
+++ src/homepage.js
@@ -1,8 +1,7 @@
 import italianPennant from"./images/italy_pennant.png";
 import clockIcon from "./images/chef-hat.png";
-import italianEsaGold from "./images/italy_esa_gold.png";
 import italianGpsIcon from "./images/italy_gps.png";
-
+import { setTitle } from "./index.js";
 
 function loadHomePage(){
 
@@ -15,9 +14,7 @@ for(let child of divChildren){
 }
 //crea elementi della Homepage:
 //header
-const title = document.createElement("div");
-const logo = document.createElement("img");
-const TitleHeader = document.createElement("h1");
+
 //descrizione
 const description = document.createElement("div");
 const pennant = document.createElement("img");
@@ -35,9 +32,7 @@ const locationText = document.createElement("p");
 
 
 //modifica classi
-title.classList.add("title");
-logo.classList.add("logo");
-TitleHeader.classList.add("titleHeader");
+
 description.classList.add("homepageDiv", "description", "divChildDynamic");
 pennant.classList.add("absoluteIcon");
 descriptionText.classList.add("normalText");
@@ -50,26 +45,24 @@ gpsIcon.classList.add("absoluteIcon");
 locationTitle.classList.add("subHeader");
 locationText.classList.add("normalText");
 //modifica attributi
-logo.setAttribute("src", italianEsaGold);
+
 pennant.setAttribute("src", italianPennant);
 clock.setAttribute("src", clockIcon);
 gpsIcon.setAttribute("src", italianGpsIcon);
 //modifica testo
-TitleHeader.textContent = 'Ristorante Pizzeria "La Vera Italia"';
+setTitle('Ristorante Pizzeria "La Vera Italia"');
 descriptionText.textContent = "Template della descrizione, testo casuale, blah blah blah blah e ancora blah e poi blah, ciao mondo";
 staffSignature.textContent = '-Lo Staff de "La Vera Italia"';
 hourTitle.textContent = "Hours";
 locationTitle.textContent= "Location";
 locationText.textContent = "789 Colosseum Avenue, Augustus City, Your State";
 //aggiunta elementi
-contentDiv.appendChild(title);
+
 contentDiv.appendChild(description);
 contentDiv.appendChild(hours);
 contentDiv.appendChild(location);
 //aggiunta header
-title.appendChild(logo);
-title.appendChild(TitleHeader);
-title.appendChild(logo.cloneNode(true));
+
 //aggiunta descrizione
 description.appendChild(pennant);
 description.appendChild(descriptionText);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,65 @@
 import "./modern-normalize.css";
 import "./style.css";
 import loadHomepage from "./homepage.js";
+import italianEsaGold from "./images/italy_esa_gold.png";
 
+//carica homepage all'apertura del sito
+createTitle();
 loadHomepage();
 
+
+
+
+
+
+
+
+
+
+
+
+//ricerca elementi DOM
+const buttonNav = document.querySelector("#mainNav");
+
+//event listeners
+buttonNav.addEventListener("click", (e)=>{
+    let element = e.target;
+
+    switch(element.id){
+        case "homeButton":
+            loadHomepage();
+            console.log("hmepage loaded");
+            break;
+    }
+});
+
+//funzioni:
+function createTitle(){
+    //creazione elementi
+    const contentDiv = document.querySelector("#content");
+    const title = document.createElement("div");
+    const logo = document.createElement("img");
+    const TitleHeader = document.createElement("h1");
+    //modifica classi
+    title.classList.add("title");
+    logo.classList.add("logo");
+    TitleHeader.classList.add("titleHeader");
+    //modifica attributi
+    logo.setAttribute("src", italianEsaGold);
+    //aggiunge l'elemento;
+    contentDiv.appendChild(title);
+    title.appendChild(logo);
+    title.appendChild(TitleHeader);
+    title.appendChild(logo.cloneNode(true));
+}
+
+function setTitle(newTitle){
+    const titleHeader = document.querySelector(".titleHeader");
+    titleHeader.textContent = newTitle;
+    console.log("Titolo cambiato");
+}
+
+
+
+
+export {setTitle};
